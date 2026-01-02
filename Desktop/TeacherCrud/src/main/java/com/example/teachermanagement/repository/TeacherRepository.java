f7efac456261ca0e17cc2bb0b462a4b79c792360
Prateek Rai <prateekrai903@gmail.com>
2026-01-02 13:47:19 +0530

    Delete Desktop/TeacherCrud directory

--- Desktop/TeacherCrud/src/main/java/com/example/teachermanagement/repository/TeacherRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.teachermanagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.teachermanagement.model.Teacher;
-
-public interface TeacherRepository extends JpaRepository<Teacher, Long> {
-}
